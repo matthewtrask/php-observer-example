464574a941a8e6138a5c150914d2a6de1ac08e1a
André Schwarzer <andre@schwarzer.it>
2015-08-03 01:25:09 +0200

    Rearrangement

--- Example.php
+++ Example.php
@@ -4,15 +4,18 @@ spl_autoload_register(function ($class) {
     include __DIR__.'/'.$class.'.php';
 });
 
+
 // Object creation/generation
 $fuelTankWarning = new FuelTankWarning();
 $fuelDisplay = new FuelDisplay();
 
 $fuelSensor = new FuelSensor();
 
+
 // Our FuelSensor extends Observeable and handles the Observers
 $fuelSensor->addObserver($fuelTankWarning);
 $fuelSensor->addObserver($fuelDisplay);
+
 
 // The Observers are triggered by a value change
 $fuelSensor->setFuelLevel(80);
@@ -21,8 +24,15 @@ $fuelSensor->setFuelLevel(5);
 $fuelSensor->setFuelLevel(1);
 $fuelSensor->setFuelLevel(100);
 
+
+// uncommend to throw Exception
+//$fuelSensor->setFuelLevel(101);
+
+
+// Removing Observer when they are not needed anymore
 $fuelSensor->removeObserver($fuelTankWarning);
 $fuelSensor->removeObserver($fuelDisplay);
+
 
 /** This may cause an error log entry like this:
  *  [DATE TIME] [error] [client IP] PHP Notice:  Undefined variable: thisIsNotExisting in Example.php on line 26
@@ -30,6 +40,3 @@ $fuelSensor->removeObserver($fuelDisplay);
  *  removeObserver is just working fine.
  */
 //$fuelSensor->removeObserver($thisIsNotExisting);
-
-// uncommend to throw Exception
-//$fuelSensor->setFuelLevel(101);
